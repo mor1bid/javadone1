38b21ea8fe79eb6b812d9cdbac1820552185453a
De <spark49942@gmail.com>
2022-11-23 04:46:46 +0300

    NO7

--- dz.java
+++ dz.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import javax.lang.model.util.ElementScanner14;
-
 public class dz {
     public static void main(String[] args) {
         Scanner work = new Scanner(System.in);
@@ -43,47 +41,6 @@ public class dz {
 		else if (opera.equals("/")) { int res = numa / numb; System.out.println("Ответ: " + res); }
 		else System.out.println("ошибка");
 
-	// 	String ask = work.nextLine();
-	// 	// ask += "+1";
 		work.close();
-	// 	String[] mask = ask.split(" ");
-	// 	String digray[] = new String[mask.length];
-	// 	String dig = "";
-	// 	for (int i = 0, j = 0; i < mask.length; i++) {
-	// 		if (mask[i].chars().allMatch(Character::isDigit)) {
-	// 			dig += mask[i];
-	// 		}
-	// 		else {
-	// 			digray[j] = dig;
-	// 			digray[j+1] = mask[i];
-	// 			dig = "";
-	// 			j++;
-	// 		}
-	// 	}
-	// 	System.out.println(digray[1]);
-	// 	int answ = Integer.parseInt(digray[0]);
-	// 	int firnum = 0;
-	// 	for (int i = 0; i < digray.length - 1; i++) {
-	// 		if (digray[i+1].equals("*") || digray[i+1].equals("/")) {
-	// 			firnum = Integer.parseInt(digray[i]);
-	// 		}
-	// 	}
-	// 	for (int i = 0; i < digray.length - 1; i++) {
-	// 		if (digray[i+1].equals("*")) {
-	// 			firnum *= Integer.parseInt(digray[i+2]);
-	// 		}
-	// 		if (digray[i+1].equals("/")) {
-	// 			firnum /= Integer.parseInt(digray[i+2]);
-	// 		}
-	// 	}
-	// 	for (int i = 0; i < digray.length - 1; i++) {
-	// 		if (digray[i+1].equals("+")) {
-	// 			firnum += Integer.parseInt(digray[i+2]);
-	// 		}
-	// 		if (digray[i+1].equals("-")) {
-	// 			firnum -= Integer.parseInt(digray[i+2]);
-	// 		}
-	// 	}
-	// 	System.out.println(firnum);
 		}
     }
